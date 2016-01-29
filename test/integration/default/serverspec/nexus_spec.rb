47e9c1b1f7ddd98422ff628f95eb4fa4e9fe8387
Tobias Wallura <tobias@wallura.org>
2016-01-29 22:21:56 +0100

    add sleep to tests to allow service to start

--- test/integration/default/serverspec/nexus_spec.rb
+++ test/integration/default/serverspec/nexus_spec.rb
@@ -7,8 +7,13 @@ describe service("nexus") do
   it { should be_running }
 end
 
-describe host("localhost") do
-  it { should be_reachable.with( :port => 8081, :timeout => 10 ) }
+describe command("sleep 20s") do
+  # let service start
+  its(:exit_status) { should eq 0 }
+end
+
+describe port("8081") do
+  it { should be_listening }
 end
 
 describe command("curl -L localhost:8081") do
